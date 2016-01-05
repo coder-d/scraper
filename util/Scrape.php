2cb79c1e8fce69492e7a6915dabd290c77ef11b4
debanjan <debanjan@browntape.com>
2016-01-05 20:17:29 +0530

    updates

--- util/Scrape.php
+++ util/Scrape.php
@@ -1,64 +1,59 @@
 <?php
-	class Scrape
-{
+class Scrape{
+	
 	public function getRemoteData($title){
 		$amazonContent = $this->_amazonData($title);
 		$ebayContent = $this->_ebayData($title);
 		$mergedContent = array_merge($amazonContent, $ebayContent);
-		//var_dump($amazonContent);
 		return $mergedContent;
-
 	}
 
 	private function _amazonData($title){
 		$contents = $this->_remoteCall('amazon',$title);
-	    // DOM document Creation
-	    $dom = new DOMDocument;
-	    @$dom->loadHTML($contents);
-	    $xpath = new DOMXPath($dom);
-	    $links = @$xpath->query('//*[contains(concat(" ", normalize-space(@id), " "), "s-results-list-atf")]//li');
-	  	$fetchedData = array();
-	    for ($i = 0; $i < ($links->length); $i++){
-	    	$itemLink = $xpath->query('//*[contains(concat(" ", normalize-space(@class), " "), "s-access-detail-page")]', $links->item($i));
-	    	$linkTitle = @$itemLink->item($i)->nodeValue;
-	    	if(is_object($itemLink->item($i))){
-		    	$linkHref = @$itemLink->item($i)->getAttribute('href');
-		    	$itemPrice = @$xpath->query('//*[contains(concat(" ", normalize-space(@class), " "), "s-price")]', $links->item($i));
-		    	$price = @$itemPrice->item($i)->nodeValue;
-		    	$fetchedData[$i.'amazon']['title'] = html_entity_decode($linkTitle);
-		    	$fetchedData[$i.'amazon']['href'] = $linkHref;
-		    	$fetchedData[$i.'amazon']['price'] = $price;
-	    	}
-	    			
+		// DOM document Creation
+		$dom = new DOMDocument;
+		@$dom->loadHTML($contents);
+		$xpath = new DOMXPath($dom);
+		$links = @$xpath->query('//*[contains(concat(" ", normalize-space(@id), " "), "s-results-list-atf")]//li');
+		$fetchedData = array();
+		for ($i = 0; $i < ($links->length); $i++){
+		    $itemLink = $xpath->query('//*[contains(concat(" ", normalize-space(@class), " "), "s-access-detail-page")]', $links->item($i));
+		    $linkTitle = @$itemLink->item($i)->nodeValue;
+		    if(is_object($itemLink->item($i))){
+			    $linkHref = @$itemLink->item($i)->getAttribute('href');
+			    $itemPrice = @$xpath->query('//*[contains(concat(" ", normalize-space(@class), " "), "s-price")]', $links->item($i));
+			    $price = @$itemPrice->item($i)->nodeValue;
+			    $fetchedData[$i.'amazon']['title'] = html_entity_decode($linkTitle);
+			    $fetchedData[$i.'amazon']['href'] = $linkHref;
+			    $fetchedData[$i.'amazon']['price'] = $price;
+		    }			
 		}
 		return $fetchedData;
 	}
 
 	private function _ebayData($title){
 		$contents = $this->_remoteCall('ebay',$title);
-	    // DOM document Creation
-	    $dom = new DOMDocument;
-	    @$dom->loadHTML($contents);
-	    $xpath = new DOMXPath($dom);
-	    $links = @$xpath->query('//*[contains(concat(" ", normalize-space(@id), " "), "ListViewInner")]//li
-	    		[contains(concat(" ", normalize-space(@class), " "), "sresult")]');
-	    $fetchedData = array();
-	    for ($i = 0; $i < ($links->length); $i++){
-	    	$itemLink = @$xpath->query('//*[contains(concat(" ", normalize-space(@class), " "), "lvtitle")]//a', $links->item($i));
-	    	if(is_object($itemLink->item($i))){
-	    		$linkTitle = @$itemLink->item($i)->nodeValue;
-	    		$linkHref = @$itemLink->item($i)->getAttribute('href');	
-	    		$itemPrice = @$xpath->query('//*[contains(concat(" ", normalize-space(@class), " "), "lvprice")]//
-	    		span[contains(concat(" ", normalize-space(@class), " "), "bold")][1]', $links->item($i));
-		    	$price = @$itemPrice->item($i)->nodeValue;
-		    	$fetchedData[$i.'ebay']['title'] = html_entity_decode($linkTitle);
-		    	$fetchedData[$i.'ebay']['href'] = $linkHref;
-		    	$fetchedData[$i.'ebay']['price'] = $price;	
-	    	}
-	    		
+		// DOM document Creation
+		$dom = new DOMDocument;
+		@$dom->loadHTML($contents);
+		$xpath = new DOMXPath($dom);
+		$links = @$xpath->query('//*[contains(concat(" ", normalize-space(@id), " "), "ListViewInner")]//li
+		    	[contains(concat(" ", normalize-space(@class), " "), "sresult")]');
+		$fetchedData = array();
+		for ($i = 0; $i < ($links->length); $i++){
+		    $itemLink = @$xpath->query('//*[contains(concat(" ", normalize-space(@class), " "), "lvtitle")]//a', $links->item($i));
+		    if(is_object($itemLink->item($i))){
+		    	$linkTitle = @$itemLink->item($i)->nodeValue;
+		    	$linkHref = @$itemLink->item($i)->getAttribute('href');	
+		    	$itemPrice = @$xpath->query('//*[contains(concat(" ", normalize-space(@class), " "), "lvprice")]//
+		    	span[contains(concat(" ", normalize-space(@class), " "), "bold")][1]', $links->item($i));
+			    $price = @$itemPrice->item($i)->nodeValue;
+			    $fetchedData[$i.'ebay']['title'] = html_entity_decode($linkTitle);
+			    $fetchedData[$i.'ebay']['href'] = $linkHref;
+			    $fetchedData[$i.'ebay']['price'] = $price;	
+		    }		
 		}
 		return $fetchedData;
-
 	}
 
 	private function _remoteCall($site,$title){
@@ -72,11 +67,7 @@
 			break;
 		}
 		// Get the HTML Source Code
-	    $contents= file_get_contents($url);
-	    return $contents;
+		$contents= file_get_contents($url);
+		    return $contents;
 	}
-
-
-
-
 }
